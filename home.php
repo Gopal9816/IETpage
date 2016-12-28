9ddd48e006113d4f596ebac24ecff9cc7304f876
Gopal <gopalkurup@gmail.com>
2016-12-28 12:12:29 +0530

    Home.php modified

--- home.php
+++ home.php
@@ -15,7 +15,7 @@ $DBcon->close();
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Welcome - <?php echo $userRow['email']; ?></title>
+<title>Welcome - <?php echo $userRow['username']; ?></title>
 
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"> 
 <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen"> 
@@ -33,14 +33,9 @@ $DBcon->close();
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="http://www.codingcage.com">Coding Cage</a>
+          <a style="font-size: 32px; text-decoration-style: -moz-none;">DASHBOARD</a>
         </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="http://www.codingcage.com/2015/03/simple-login-and-signup-system-with-php.html">Back to Article</a></li>
-            <li><a href="http://www.codingcage.com/search/label/jQuery">jQuery</a></li>
-            <li><a href="http://www.codingcage.com/search/label/PHP">PHP</a></li>
-          </ul>
+        <div id="navbar" class="navbar-collapse collapse">          
           <ul class="nav navbar-nav navbar-right">
             <li><a href="#"><span class="glyphicon glyphicon-user"></span>&nbsp; <?php echo $userRow['username']; ?></a></li>
             <li><a href="logout.php?logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp; Logout</a></li>
@@ -49,9 +44,10 @@ $DBcon->close();
       </div>
     </nav>
 
-<div class="container" style="margin-top:150px;text-align:center;font-family:Verdana, Geneva, sans-serif;font-size:35px;">
-	<a href="http://www.codingcage.com/">Coding Cage - Programming Blog</a><br /><br />
-    <p>Tutorials on PHP, MySQL, Ajax, jQuery, Web Design and more...</p>
+<div class="container" style="margin-top:100px;text-align:left;font-family:Verdana, Geneva, sans-serif;">
+	<h1>WELCOME <?php echo $userRow['username'];?></h1><br /><br />
+    <p style="font-size:20px;">e-mail : <?php echo $userRow['email'];?><br />IET membership number :</p>
+    
 </div>
 
 </body>
